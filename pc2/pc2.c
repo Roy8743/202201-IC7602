11b98af048a901c1c3df57eabf87916f6198b26a
roy8743 <roychavarriagarita@gmail.com>
2022-03-11 17:53:09 -0600

    Agrego forma de recibir parametros

---
+++ pc2/pc2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+-Para compilar el archivo:  gcc pc2.c -o hamming
+-Para correr la aplicacion: ./hamming encode --input 2
+*/
+
+int main(int argc, char *argv[]) {
+	
+	printf("\n%s",argv[3]);
+	return 0;
+}

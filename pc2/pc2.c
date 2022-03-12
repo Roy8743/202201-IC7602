881de5508597bfd189fbf68d074460d6d2be8f84
roy8743 <roychavarriagarita@gmail.com>
2022-03-11 22:25:41 -0600

    Agrefo pc2

--- pc2/pc2.c
+++ pc2/pc2.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 /*
 -Para compilar el archivo:  gcc pc2.c -o hamming
 -Para correr la aplicacion: ./hamming encode --input 2
 */
 
+/*int code()*/
+
+
+	//0x3E465C
+
 int main(int argc, char *argv[]) {
-	
-	printf("\n%s",argv[3]);
+
+	int num;
+	sscanf(argv[3], "%x", &num);
+
+	printf("0x%x %i\n", num, num);
+	printf("%s", ~001);
+
+	//3E = 111110
+	//printf("\n%b", num);
+
+
 	return 0;
 }
